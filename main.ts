92f07bc9330a4eb56f1a453881e2755ba0cfc18c
fishiversity <41026299+fishiversity@users.noreply.github.com>
2023-03-14 13:44:51 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
     radio.sendString("E")
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
     radio.sendString("F")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
 })
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
     radio.sendString("D")
@@ -10,6 +16,7 @@ joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
     radio.sendString("C")
 })
+joystickbit.initJoystickBit()
 radio.setGroup(1)
 basic.showIcon(IconNames.Yes)
 basic.showString("J")
